f763366cfca58e1baceb0c182382edb019ba616a
Hemant Ashta <hemantashta@Hemants-MacBook-Air.local>
2024-04-21 03:41:44 +0530

    added: unit test cases for the project

--- app/routers/user.py
+++ app/routers/user.py
@@ -11,7 +11,6 @@ router = APIRouter(
 
 @router.post('/', status_code = status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db.exe
     hash_pass = utils.hash(user.password)
     user.password = hash_pass
 
